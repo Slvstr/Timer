3fe1fef2618cb9926ba2e9e6610ec880fae5cb5b
Charles Walton <charleswalton@google.com>
2014-03-05 14:38:45 -0800

    Adding CSS framework.

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Configuration for Compass CSS framework:
+# http://compass-style.org/
+#
+# Quick install:
+# 1) Get ruby:
+#    sudo apt-get install ruby1.9.1
+# 2) Get SASS and Compass:
+#    sudo gem install sass
+#    sudo gem install compass
+# 3) Done!
+#
+# To compile SCSS from root:
+# compass compile ./
+#
+# To automatically watch and recompile project on save:
+# compass watch ./
+
+http_path = "/"
+css_dir = "css/compiled"
+sass_dir = "css"
+images_dir = "images"
+javascripts_dir = "js"
+
+# Set minification level, can be: :expanded, :nested, :compact, :compressed
+output_style = :compressed
