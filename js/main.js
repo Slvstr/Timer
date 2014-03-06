1994bbc8d92a41e8ab944e426da8dead04ab7627
Charles Walton <charleswalton@google.com>
2014-03-05 16:14:38 -0800

    Initial timing logic.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("hello!");
+var TimerApp = angular.module('Timer', []);
+
+TimerApp.controller('MainCtrl', function($scope, $interval) {
+
+  $scope.TotalTime = 5;
+  $scope.TimeRemaining = 0;
+
+  var timer;
+
+  // Creates global timer and kicks it off.
+  $scope.StartTimer = function(e) {
+    $scope.TimeRemaining = $scope.TotalTime;
+
+    timer = $interval(function() {
+      if ($scope.TimeRemaining <= 0) {
+        $scope.StopTimer();
+
+      } else {
+        $scope.TimeRemaining = $scope.TimeRemaining - 1;
+      }
+    }, 1000);
+  };
+
+  // Kills timer.
+  $scope.StopTimer = function() {
+    $interval.cancel(timer);
+  };
+
+});
